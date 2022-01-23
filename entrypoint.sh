27ba5d72ff27a316b5a86f5b117999924044f27f
shneezin <sebastian.kraehe@googlemail.com>
2022-01-23 05:03:11 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ echo "Run once: ${RUN_ONCE}"
 if [ "$RUN_ONCE" = "false" ]; then
 printenv | sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
 echo "Setting cron schedule: ${CRON_SCHEDULE}"
-echo "${CRON_SCHEDULE} /bin/bash -c '/usr/local/bin/node /usr/src/app/index.js' > /proc/1/fd/1 2>/proc/1/fd/2" | crontab -
+echo "${CRON_SCHEDULE} /bin/bash -c 'python3 /usr/src/app/speedtest.py' > /proc/1/fd/1 2>/proc/1/fd/2" | crontab -
 crond -f
 else
 node /usr/src/app/index.js
